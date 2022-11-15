c8e6bec6a9d781bbe3a801098acfa22debbe1855
ibnu choldun <ibnucholdun023@gmail.com>
2022-11-15 21:45:51 +0700

    create redux thunk for sign up page method post

--- src/Components/InputText/InputText.jsx
+++ src/Components/InputText/InputText.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const InputText = ({valueLabel, valueFor, type, id, name, placeholder, minLength}) => {
+const InputText = ({valueLabel, valueFor, type, id, name, placeholder, minLength, value, onChange}) => {
     return (
         <div className="mb-3">
             <label htmlFor={valueFor} className="form-label mb-0">{valueLabel}</label>
-            <input type={type} className="form-control" id={id} name={name} placeholder={placeholder} required minLength={minLength}/>
+            <input type={type} className="form-control" id={id} name={name} placeholder={placeholder} required minLength={minLength} value={value} onChange={onChange}/>
         </div>
     )
 }
@@ -17,7 +17,8 @@ InputText.propTypes = {
     id: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     placeholder: PropTypes.string.isRequired,
-    minLength: PropTypes.number
+    minLength: PropTypes.number,
+    value: PropTypes.string
 }
 
 export default InputText
